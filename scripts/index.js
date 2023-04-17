e910a0c26761696c2979c5ca6961ea9e90e1ba3e
Vladimir Efimov <pro100oper@gmail.com>
2023-04-17 17:02:33 +0300

    5-й спринт

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,110 @@
-let backgroundPopup = document.querySelector('.popups');
-let editProfileButton = document.querySelector('.profile__edit-button');
-let profile = document.querySelector('.profile__info')
-let form = document.querySelector('.popup__form');
-let closePopup = document.querySelector('.popup__close-bttn');
-// Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__input_type_name');// Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector('.popup__input_type_job'); // Воспользуйтесь инструментом .querySelector()
-let nameProfile = profile.querySelector('.profile__name');
-let jobProfile = profile.querySelector('.profile__job');
+// Редактирование профиля
+const editPopup = document.querySelector('.popups_type_edit'); //Окно редактирования
+const editForm = editPopup.querySelector('.popup__form');
+const editClosePopup = editPopup.querySelector('.popup__close-bttn');
+const editNameInput = editPopup.querySelector('.popup__input_type_name');
+const editJobInput = editPopup.querySelector('.popup__input_type_job'); 
 
-function closeEdit() {
-    backgroundPopup.classList.remove('popup_opened');
-}
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+//Добавление карточки 
+const addPopup = document.querySelector('.popups_type_add'); //Окно добавления
+const addForm = addPopup.querySelector('.popup__form');
+const addClosePopup = addPopup.querySelector('.popup__close-bttn');
+const addNameInput = addPopup.querySelector('.popup__input_type_name');
+const addUrlInput = addPopup.querySelector('.popup__input_type_url');
+//Кнопка добавления карточки
+const addCardButton = document.querySelector('.profile__add-post-button');
+
+
+//Постоянные данных профиля
+const profile = document.querySelector('.profile__info');
+const nameProfile = profile.querySelector('.profile__name');
+const jobProfile = profile.querySelector('.profile__job');
+//Поятоянная кнопки редактирования профиля
+const editProfileButton = document.querySelector('.profile__edit-button');
+
+
+
+//Постоянные попап с картинкой
+const popupImage = document.querySelector('.popups_type_image');
+const closePopupImage = popupImage.querySelector('.popup__close-bttn');
+const imgPopupImage = popupImage.querySelector('.popup__image');
+const subtitlePopupImage = popupImage.querySelector('.popup__subtitle');
+
+
+//Обработчик формы редактирования пролфиля
 function handleFormSubmit (evt) {
-   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-
-    // Получите значение полей jobInput и nameInput из свойства value
-let namePop = nameInput.value;
-let jobPop = jobInput.value; 
-    // Выберите элементы, куда должны быть вставлены значения полей
-nameProfile = profile.querySelector('.profile__name');
-jobProfile = profile.querySelector('.profile__job');
-    // Вставьте новые значения с помощью textContent
-nameProfile.textContent = namePop;
-jobProfile.textContent = jobPop;
-closeEdit();
+   evt.preventDefault(); 
+    nameProfile.textContent = editNameInput.value;
+    jobProfile.textContent = editJobInput.value;
+    delPopup(editPopup);
 }
 
-function openEdit() {
-    backgroundPopup.classList.add('popup_opened');
-    nameInput.value = nameProfile.textContent;
-    jobInput.value = jobProfile.textContent; 
+
+const cardList = document.querySelector('.cards');
+const cardTemplate = document.querySelector('.cards-template').content;
+//Создание карты карточки
+ function createCard(element) { 
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__image').src = element.link;
+    cardElement.querySelector('.card__image').alt = element.name;
+    cardElement.querySelector('.card__title').textContent = element.name;
+    cardElement.querySelector('.card__like').addEventListener('click',(event) => {
+        event.target.classList.toggle('card__like_active');
+    });
+    //Обработчик на удаление
+    cardElement.querySelector('.card__trash').addEventListener('click', (cadrdDelite) => {
+      cardElement.remove();
+    });
+    //Обработчик на попап с картинкой (закрытие и открытие)
+    cardElement.querySelector('.card__image').addEventListener('click',(pushPopupImage) => {
+      showPopup(popupImage);
+      imgPopupImage.src = cardElement.querySelector('.card__image').src;
+      imgPopupImage.alt = cardElement.querySelector('.card__image').alt; 
+      subtitlePopupImage.textContent = cardElement.querySelector('.card__title').textContent;
+        });
+   return cardElement;
+  };
+
+
+//Добавление карточки
+initialCards.forEach((objectCard) => {
+    const newCard = createCard(objectCard);
+    cardList.append(newCard);
+});
+
+
+//Функция вызова формы добавления карточки
+function handleFormAddCard(evt) {
+    evt.preventDefault();
+    const newObjectCard = {
+        name: addNameInput.value,
+        link: addUrlInput.value
+    };
+    addNameInput.value = '';
+    addUrlInput.value = '';
+    cardList.prepend(createCard(newObjectCard));
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-form.addEventListener('submit', handleFormSubmit);
-editProfileButton.addEventListener('click', openEdit);
-closePopup.addEventListener('click', closeEdit);
+function showPopup(popup) {
+    popup.classList.add('popup_opened');
+}
+
+function delPopup(popup) {
+    popup.classList.remove('popup_opened');
+}
+
+editProfileButton.addEventListener('click',() => showPopup(editPopup));
+
+editClosePopup.addEventListener('click',() => delPopup(editPopup));
+closePopupImage.addEventListener('click',() => {
+  delPopup(popupImage);
+});
+
+addCardButton.addEventListener('click', () => showPopup(addPopup));
+addClosePopup.addEventListener('click', () => delPopup(addPopup));
+
+
+// Cлушатели нажатия кнопок
+editForm.addEventListener('submit', handleFormSubmit);
+addForm.addEventListener('submit',handleFormAddCard);
